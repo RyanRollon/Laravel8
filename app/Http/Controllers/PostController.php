a2fda2145f0cd51a0fda018013912bd0672840b6
Laravel8 <ryanrollon.hs@gmail.com>
2022-01-13 09:49:49 +0800

    okey

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,20 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
+
 class PostController extends Controller
 {
    
     public function index()
     {
 
-        //return view('posts.index');
-
-        $posts = Post::paginate(2);
+        // return view('posts.index');
+        $posts = Post::paginate(3);
         return view('posts.index', [
             'posts' => $posts
         ]);
 
-        
     }
    
     public function store(Request $request)
@@ -30,14 +29,14 @@ class PostController extends Controller
         //dd(auth()->user()->posts);
         //return view('dashboard');
 
-
        $request->user()->posts()->create([
         'body' => $request->body
        ]);
 
         return back();
-        
+
     }
 
+  
     
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,7 +18,7 @@
 <div class="container my-5 py-5 px-5">
         <div class="row justify-content-center">
            <div class="col-sm-7 bg-dark">
-                <div class="panel ">
+                <div class="panel">
                     <form action="{{  route('posts.index') }}" method="post">
                           @csrf
                          <textarea class="form-control" name="body" id="body" placeholder="Write a comment..." rows="4"> 
@@ -37,14 +37,28 @@
                         <a href="" style="color:white; text-decoration:none"><b> {{ $post->user->name }} </b></a>
                         <span style="color:grey"> {{ $post->created_at->diffForHumans() }}</span>
                         
-                        <p style="color:white"> {{ $post->body }}</p>
+                        <p style="color:white">
+                          <tr>
+                           <td>{{ $post->body }}</td>
+                          </tr>
+                         </p>
                       </div>
                      @endforeach
+                     
                  @else
+                    
                     <h6 style="color:white"> There is no posts</h6>
-                 @endif
+                 @endif  
+                 
+                 {{ $posts->links() }}
                 </div>
+              
+               
+                
            </div>
          </div>
+         
+  
 </div>
+
 @endsection
